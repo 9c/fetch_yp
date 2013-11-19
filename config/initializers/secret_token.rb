820de7b9b03a69c9d288ce8c4f24934578ce1750
Ken Lu <ken@seeice.com>
2013-11-19 15:16:28 -0500

    init the yp project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yp::Application.config.secret_key_base = '527e9623e81029c15bd1c7e22a6ef292455d10b7277870d8907b17c29be75e65f510d5feb9f47e5bd57d04b5c9cf97ab579f05a1f05a576007d4c962062f8524'
