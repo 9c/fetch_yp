ec5cbf7a840ff05dab5fe9574d1640cafddc9d22
Ken Lu <ken@seeice.com>
2013-11-20 08:46:59 -0500

    updated the function

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,24 +3,28 @@ PAGE = 10
 class Restaurant < ActiveRecord::Base
   def Restaurant::grasp
     count = 0
-    arr_list = Array.new
     PAGE.times.each do |page|
       doc = Nokogiri::HTML(open('http://www.yellowpages.com/new-york-ny/restaurants?g=New+York%2C+NY&page=' + (page + 1).to_s))
       doc.css('.listing-content').each do |content|
-        unit = Hash.new
-        business_name = content.css('.business-name a')
-        unit[:detail_url] = business_name.attr('href').value
-        unit[:title] = business_name.attr('title').value
-        unit[:unique_code] = content.css('.business-name').data('lid')
-        unit[:phone_num] = content.css('.business-phone').text.strip.delete("\n\t")
-        unit[:address] = content.css('.listing-address').text.strip.delete("\n\t")
-        unit[:street] = content.css('.listing-address .street-address').text.strip.delete("\n\t")
-        unit[:locality] = content.css('.listing-address .locality').text.strip.delete("\n\t")
-        unit[:region] = content.css('.listing-address .region').text.strip.delete("\n\t")
-        unit[:postal_code] = content.css('.listing-address .postal-code').text.strip.delete("\n\t")
+        unique_code = content.css('.business-name').data('lid')
+        unless Restaurant.exists?(unique_code: unique_code)
+          rt = Restaurant.new
+          business_name = content.css('.business-name a')
+          rt.detail_url = business_name.attr('href').value
+          rt.title = business_name.attr('title').value
+          rt.unique_code = unique_code 
+          rt.phone_num = content.css('.business-phone').text.strip.delete("\n\t")
+          rt.address = content.css('.listing-address').text.strip.delete("\n\t")
+          rt.street = content.css('.listing-address .street-address').text.strip.delete("\n\t")
+          rt.locality = content.css('.listing-address .locality').text.strip.delete("\n\t")
+          rt.region = content.css('.listing-address .region').text.strip.delete("\n\t")
+          rt.postal_code = content.css('.listing-address .postal-code').text.strip.delete("\n\t")
+          rt.save
+          count += 1
+        end
       end
     end
-
+    count
   end
 
 end
